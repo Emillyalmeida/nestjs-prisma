ff0c14e0d8ce52106b07cdb5829886512a499c3d
Emillyalmeida <emillyalmeidan@gmail.com>
2022-12-16 00:29:12 -0300

    first class concluded

---
+++ src/create-notification-body.ts
@@ -0,0 +1,16 @@
+import { IsNotEmpty, IsUUID, Length } from "class-validator"
+
+export class CreateNotificationBody {
+
+    @IsNotEmpty()
+    @IsUUID()
+    recipientId: string
+
+    @IsNotEmpty()
+    @Length(5,300)
+    content: string
+
+    @IsNotEmpty()
+    category: string
+
+}
